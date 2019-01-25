3bc5417d6f164ae645c7e233e8ff59d96de46802
xbb <12990798xbb@gmail.com>
2019-01-25 12:57:02 +0800

    add population

---
+++ highs_lows.py
@@ -0,0 +1,41 @@
+import csv
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+filename = 'sitka_weather_2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # for index, column_header in enumerate(header_row):
+    #   print(index, column_header)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        try:
+            high = int(row[1])
+            current_date = datetime.strptime(row[0], "%Y/%m/%d")
+            low = int(row[3])
+        except ValueError:
+            print('missing data exist')
+        else:
+            highs.append(high)
+            dates.append(current_date)
+            lows.append(int(low))
+
+    # print(highs)
+
+# 根据数据绘制图形
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# 设置图形的格式
+plt.title("Daily high and low temperatures, 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature(F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
