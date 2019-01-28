432f85dcc34399f724590313141ab36faab088f8
xbb <12990798xbb@gmail.com>
2019-01-28 16:54:02 +0800

    add population map

--- world_population.py
+++ world_population.py
@@ -1,13 +1,43 @@
 import json
 
+from country_codes import get_country_code
+import pygal_maps_world.maps
+from pygal.style import RotateStyle, LightSolarizedStyle
+
 # 将数据加载到一个列表中
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
+# 创建一个键为国标码，值为人口的字典
+cc_populations = {}
+
 # 打印每个国家2010年的人口数量
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
-        population = pop_dict['Value']
-        print(country_name + ":" + population)
+        population = int(float((pop_dict['Value'])))
+        # print(country_name + ":" + str(population))
+        code = get_country_code(country_name)
+        if code:
+            cc_populations[code] = population
+
+# 根绝人口数量将所有的国家分成三组
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+# print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+wm_style = RotateStyle('#336699', base_style=LightSolarizedStyle)
+wm = pygal_maps_world.maps.World(style=wm_style)
+wm.title = 'World Population in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+
+wm.render_to_file('world_population.svg')
